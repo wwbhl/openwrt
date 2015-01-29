2aec0581addd0cf1820af7ac2696915d40127f7e
wwbhl <baohailong@baohailongs-MacBook-Pro.local>
2015-01-29 09:27:09 +0800

    extra

--- luci/applications/luci-aria2/luasrc/controller/aria2.lua
+++ luci/applications/luci-aria2/luasrc/controller/aria2.lua
@@ -1,22 +1,23 @@
 --[[
-wwbhl
+LuCI - Lua Configuration Interface - aria2 support
+
+Copyright 2014 nanpuyue <nanpuyue@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
 ]]--
 
 module("luci.controller.aria2", package.seeall)
 
 function index()
-	
 	if not nixio.fs.access("/etc/config/aria2") then
 		return
 	end
 
-	local page
-	page = node("admin", "wwbhl")
-	page.target = firstchild()
-	page.title = _("wwbhl")
-	page.order  = 65
-	
-	page = entry({"admin", "wwbhl", "aria2"}, cbi("aria2"), _("aria2"), 10)
-	page.i18n = "aria2"
+	local page = entry({"admin", "services", "aria2"}, cbi("aria2"), _("Aria2 Settings"))
 	page.dependent = true
+
 end
