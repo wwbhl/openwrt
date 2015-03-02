01387c01e95baabdee29aaed90de11043166ed94
wwbhl <baohailong@baohailongs-MacBook-Pro.local>
2015-03-02 17:08:49 +0800

    wwbhl

--- package/syncy/files/usr/lib/lua/luci/controller/syncy.lua
+++ package/syncy/files/usr/lib/lua/luci/controller/syncy.lua
@@ -30,6 +30,6 @@ function index()
 	page.target = firstchild()
 	page.tatle = _("wwbhl")
 	page.order = 65
-	page = entry({"admin", "services", "syncy"}, cbi("syncy"), _("百度云同步"))
+	page = entry({"admin", “wwbhl”, "syncy"}, cbi("syncy"), _("百度云同步"))
 	page.dependent = true
 end
